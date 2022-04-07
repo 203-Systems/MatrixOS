585e7f787a81ec1be173323406c436b77fe58590
Nengzhuo Cai <cainengzhuo@gmail.com>
2022-04-07 01:35:09 -0400

    delete os in extra components

--- devices/MatrixBlock6_ESP32S3/family.cmake
+++ devices/MatrixBlock6_ESP32S3/family.cmake
@@ -3,7 +3,7 @@ cmake_minimum_required(VERSION 3.5)
 set(CMAKE_CXX_STANDARD 17)
 
 # Add os and device directories
-set(EXTRA_COMPONENT_DIRS "os" "devices/MatrixBlock6_ESP32S3/varients" "core/esp32s3")
+set(EXTRA_COMPONENT_DIRS "devices/MatrixBlock6_ESP32S3/varients" "core/esp32s3")
 include($ENV{IDF_PATH}/tools/cmake/project.cmake)
 set(SUPPORTED_TARGETS esp32s3)
 set(FAMILY_MCUS ESP32S3)
