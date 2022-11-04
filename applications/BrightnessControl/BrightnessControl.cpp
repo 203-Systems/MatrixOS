27d64e5381c65a96ce40b04b3425d4902d99f45d
Nengzhuo Cai <Null@203.io>
2022-11-04 02:51:14 -0400

    Brightness Control fix

--- applications/BrightnessControl/BrightnessControl.cpp
+++ applications/BrightnessControl/BrightnessControl.cpp
@@ -2,7 +2,7 @@
 #include "UITwoToneSelector.h"
 #include "UI4pxNumberWithColorFunc.h"
 
-void BrightnessControl::Setup() {
+void BrightnessControl::Start() {
   name = "Brightness Control";
   nameColor = Color(0xFFFFFF);
 
@@ -17,16 +17,16 @@ void BrightnessControl::Setup() {
 #endif
 
   Dimension dimension = Dimension(8, map_length / 8 + bool(map_length % 8));
-  AddUIComponent(new UITwoToneSelector(dimension, map_length, Color(0xFFFFFF), Color(0xFF0000),
-                                       (uint8_t*)&MatrixOS::UserVar::brightness.value, threshold, map,
-                                       [&](uint8_t value) -> void { MatrixOS::UserVar::brightness.Set(value); }),
+
+  UITwoToneSelector brightnessSelector(dimension, map_length, Color(0xFFFFFF), Color(0xFF0000), (uint8_t*)&MatrixOS::UserVar::brightness.value, threshold, map,
+                                       [&](uint8_t value) -> void { MatrixOS::UserVar::brightness.Set(value); });
+  AddUIComponent(brightnessSelector,
                  origin + Point(-3, 4) - Point(0, dimension.y - 1));
 
   // Number Display
-  AddUIComponent(new UI4pxNumberWithColorFunc(
-                     [&]() -> Color {
-                       return (MatrixOS::UserVar::brightness.value > threshold) ? Color(0xFF0000) : Color(0x00FF00);
-                     },
-                     3, (int32_t*)&MatrixOS::UserVar::brightness.value, Color(0xFFFFFF)),
-                 origin + Point(-4, -3));
+  UI4pxNumberWithColorFunc numDisplay([&]() -> Color { return (MatrixOS::UserVar::brightness.value > threshold) ? Color(0xFF0000) : Color(0x00FF00); }, 3,
+                                      (int32_t*)&MatrixOS::UserVar::brightness.value, Color(0xFFFFFF));
+  AddUIComponent(numDisplay, origin + Point(-4, -3));
+
+  UI::Start();
 }
