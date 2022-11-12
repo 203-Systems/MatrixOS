975435fdba90494fbf5f9f8accfb67639e50b290
Nengzhuo Cai <Null@203.io>
2022-11-12 17:45:46 -0500

    4PX number now flushes

--- os/ui/component/UI4pxNumber.h
+++ os/ui/component/UI4pxNumber.h
@@ -1,7 +1,7 @@
 #pragma once
-#include "UIComponent.h"
-#include "../data/4pxNumber.h"
 #include <cmath>
+#include "../data/4pxNumber.h"
+#include "UIComponent.h"
 
 // TODO add negative support?
 class UI4pxNumber : public UIComponent {
@@ -12,8 +12,7 @@ class UI4pxNumber : public UIComponent {
   int32_t* value;
   uint8_t spacing;
 
-  UI4pxNumber(Color color, uint8_t digits, int32_t* value, Color alternative_color = Color(0xFFFFFF),
-              uint8_t spacing = 0) {
+  UI4pxNumber(Color color, uint8_t digits, int32_t* value, Color alternative_color = Color(0xFFFFFF), uint8_t spacing = 0) {
     this->color = color;
     this->digits = digits;
     this->value = value;
@@ -25,9 +24,9 @@ class UI4pxNumber : public UIComponent {
   virtual Color GetColor() { return color; };
   virtual Color GetAlternativeColor() { return alternative_color ? alternative_color : color; };
 
-  void render4pxNumber(Point origin, Color color, uint8_t value) {
+  void Render4pxNumber(Point origin, Color color, uint8_t value) {
     // MatrixOS::Logging::LogDebug("4PX", "Num: %d, render at %d-%d", value, origin.x, origin.y);
-    if (value < 10 /*&& value >= 0*/)
+    if (value < 11 /*&& value >= 0*/)
     {
       for (int8_t x = 0; x < 3; x++)
       {
@@ -44,10 +43,12 @@ class UI4pxNumber : public UIComponent {
     for (int8_t digit = digits - 1; digit >= 0; digit--)
     {
       if (digit < sig_figure || digit == 0)
+      { Render4pxNumber(render_origin, digit % 2 ? GetAlternativeColor() : GetColor(), (int)(*value / std::pow(10, digit)) % 10); }
+      else
       {
-        render4pxNumber(render_origin, digit % 2 ? GetAlternativeColor() : GetColor(),
-                        (int)(*value / std::pow(10, digit)) % 10);
+        Render4pxNumber(render_origin, Color(0), 10);
       }
+
       render_origin = render_origin + Point(3 + spacing, 0);
     }
     return true;
