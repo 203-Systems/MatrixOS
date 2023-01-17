30336e51cb7e095305b4f1ef6d58505426725094
Nengzhuo Cai <Null@203.io>
2023-01-17 04:50:50 -0500

    HID Test app

---
+++ applications/HIDtest/HIDtest.h
@@ -0,0 +1,23 @@
+/*
+This is the example application for Matrix OS
+Remember to include this header file in the UserApplications.h in the Applications folder
+*/
+
+#pragma once
+
+#include "MatrixOS.h"
+#include "applications/Application.h"
+
+#define APPLICATION_NAME "HID Test"
+#define APPLICATION_AUTHOR "203 Electronics"
+#define APPLICATION_COLOR Color(0xFFFF00)
+#define APPLICATION_VERSION 1
+#define APPLICATION_CLASS HIDtest
+
+class HIDtest : public Application {
+ public:
+  void Setup() override;
+  void Loop() override;
+};
+
+#include "applications/RegisterApplication.h"
