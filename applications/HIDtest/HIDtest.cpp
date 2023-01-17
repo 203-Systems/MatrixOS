4ed575f13ee1d1eae11c2a9facf346ecac2f58a2
Nengzhuo Cai <Null@203.io>
2023-01-17 05:27:11 -0500

    Change keycode for HIDtest

--- applications/HIDtest/HIDtest.cpp
+++ applications/HIDtest/HIDtest.cpp
@@ -5,7 +5,7 @@ void HIDtest::Setup() {
     UI keypadUI("", Color(0xFFFFFF));
 
     keypadUI.AddUIComponent(new UIKeyboardKey(Color(0xFF0000), 0x07), Point(1, 2)); // D
-    keypadUI.AddUIComponent(new UIKeyboardKey(Color(0xFF0000), 0x08), Point(2, 2)); // F
+    keypadUI.AddUIComponent(new UIKeyboardKey(Color(0xFF0000), 0x09), Point(2, 2)); // F
 
     keypadUI.AddUIComponent(new UIKeyboardKey(Color(0xFF0000), 0x0D), Point(5, 2)); // J
     keypadUI.AddUIComponent(new UIKeyboardKey(Color(0xFF0000), 0x0E), Point(6, 2)); // K
