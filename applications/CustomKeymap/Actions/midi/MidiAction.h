b4632ee8c2e4c09bf8ec7ed2e7919399d6b480cc
Nengzhuo Cai <Null@203.io>
2023-02-19 15:08:42 -0500

    Custom Keymap Midi Action

--- applications/CustomKeymap/Actions/midi/MidiAction.h
+++ applications/CustomKeymap/Actions/midi/MidiAction.h
@@ -6,6 +6,39 @@ namespace MidiAction
 
     static bool KeyEvent(cb0r_t action, uint16_t keyID, KeyInfo* keyInfo)
     {
+        if(keyInfo->state != KeyState::PRESSED || keyInfo->state != KeyState::RELEASED || keyInfo->state != KeyState::AFTERTOUCH ) return false;
+
+        uint8_t data[3];
+
+        for(uint8_t i = 1; i < action->length; i++)
+        {
+            cb0r_s cbor_data;
+            cb0r_get(action, i, &cbor_data);
+            data[i] = action->data[i];
+        }
+
+        if(data[0] & 0xF0 == NoteOn)
+        {
+            if(data[1] >> 7)
+            {
+                data[2] = keyInfo->velocity 
+            }
+            else
+            {
+                data[2] = keyInfo->velocity == 0 ? 0 : 127;
+            }
+
+            if(keyInfo->state == KeyState::AFTERTOUCH)
+            {
+                data[0] = EMidiStatus:AfterTouch + data[0] & 0x0F; 
+            }
+        }
+        else if(keyInfo->state != KeyState::PRESSED) // Only note need to handle Release and After Touch.
+        {
+            return true;
+        }
+
+        MatrixOS::MIDI::Send(MidiPacket(0, data[0], data[0], data[1], data[2]));
         return true;
     }
 };
