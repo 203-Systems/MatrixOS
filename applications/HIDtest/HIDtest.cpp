c174587500830ffd5f62c171fea54f6e5d500a2a
Nengzhuo Cai <Null@203.io>
2023-04-10 05:10:45 -0400

    HID test app update

--- applications/HIDtest/HIDtest.cpp
+++ applications/HIDtest/HIDtest.cpp
@@ -1,14 +1,56 @@
 #include "HIDtest.h"
 #include "UIKeyboardKey.h"
+#include "UIDPad.h"
+#include "UIGamepadKey.h"
+#include "UIGamepadAxis.h"
 
 void HIDtest::Setup() {
     UI keypadUI("", Color(0xFFFFFF));
 
-    keypadUI.AddUIComponent(new UIKeyboardKey(Color(0xFF0000), KEY_D), Point(1, 2)); // D
-    keypadUI.AddUIComponent(new UIKeyboardKey(Color(0xFF0000), KEY_F), Point(2, 2)); // F
+    UIKeyboardKey Dkey(Color(0xFF0000), KEY_D);
+    keypadUI.AddUIComponent(Dkey, Point(1, 1));
 
-    keypadUI.AddUIComponent(new UIKeyboardKey(Color(0xFF0000), KEY_J), Point(5, 2)); // J
-    keypadUI.AddUIComponent(new UIKeyboardKey(Color(0xFF0000), KEY_K), Point(6, 2)); // K
+    UIKeyboardKey Fkey(Color(0xFF0000), KEY_F);
+    keypadUI.AddUIComponent(Fkey, Point(2, 1));
+
+    UIKeyboardKey Jkey(Color(0xFF0000), KEY_J);
+    keypadUI.AddUIComponent(Jkey, Point(5, 1));
+
+    UIKeyboardKey Kkey(Color(0xFF0000), KEY_K);
+    keypadUI.AddUIComponent(Kkey, Point(6, 1));
+
+    UIDPad Dpad(Color(0x00FF00));
+    keypadUI.AddUIComponent(Dpad, Point(0, 4));
+
+    UIGamepadKey Akey(Color(0x00FF00), GAMEPAD_A);
+    keypadUI.AddUIComponent(Akey, Point(6, 6));
+
+    UIGamepadKey Bkey(Color(0x00FF00), GAMEPAD_B);
+    keypadUI.AddUIComponent(Bkey, Point(7, 5));
+
+    UIGamepadKey Xkey(Color(0x00FF00), GAMEPAD_X);
+    keypadUI.AddUIComponent(Xkey, Point(5, 5));
+
+    UIGamepadKey Ykey(Color(0x00FF00), GAMEPAD_Y);
+    keypadUI.AddUIComponent(Ykey, Point(6, 4));
+
+    UIGamepadKey L1key(Color(0x00FFFF), GAMEPAD_L1);
+    keypadUI.AddUIComponent(L1key, Point(0, 3));
+
+    UIGamepadKey R1key(Color(0x00FFFF), GAMEPAD_R1);
+    keypadUI.AddUIComponent(R1key, Point(7, 3));
+
+    UIGamepadAxis L2pad(Color(0x00FFFF), GAMEPAD_AXIS_LEFT_TRIGGER, 127);
+    keypadUI.AddUIComponent(L2pad, Point(1, 3));
+
+    UIGamepadAxis R2pad(Color(0x00FFFF), GAMEPAD_AXIS_RIGHT_TRIGGER, 127);
+    keypadUI.AddUIComponent(R2pad, Point(6, 3));
+
+    UIGamepadKey Startkey(Color(0x0000FF), GAMEPAD_START);
+    keypadUI.AddUIComponent(Startkey, Point(3, 7));
+
+    UIGamepadKey Selectkey(Color(0x0000FF), GAMEPAD_SELECT);
+    keypadUI.AddUIComponent(Selectkey, Point(4, 7));    
     
     keypadUI.Start();
     Exit();
