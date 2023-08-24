01243f186058d5a52b791321df6f84a7cc23f360
Nengzhuo Cai <Null@203.io>
2023-08-23 23:02:31 -0400

    Custom Keymap action menu & menu lock

--- applications/CustomKeymap/CustomKeymap.h
+++ applications/CustomKeymap/CustomKeymap.h
@@ -1,27 +1,32 @@
 #pragma once
 
 #include "MatrixOS.h"
-#include "applications/Application.h"
 #include "UAD.h"
+#include "applications/Application.h"
+#include "UILayerControl.h"
 
 class CustomKeymap : public Application {
  public:
   static Application_Info info;
-  
+
   void Setup() override;
   void Loop() override;
-  private:
-   UAD uad;
-   void KeyEventHandler(uint16_t KeyID, KeyInfo* keyInfo);
+
+ private:
+  UAD uad;
+  bool menuLock = false;
+
+  void KeyEventHandler(uint16_t keyID, KeyInfo* keyInfo);
+  void Reload();
+  void ActionMenu();
 };
 
 inline Application_Info CustomKeymap::info = {
     .name = "Custom Keymap",
     .author = "203 Electronics",
-    .color =  Color(0xFFFF00),
+    .color = Color(0xFFFF00),
     .version = 1,
     .visibility = true,
 };
 
 REGISTER_APPLICATION(CustomKeymap);
-
