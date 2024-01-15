6e7e84330d6fa1743431f4b0ae5101194b29e075
203 Null <Null@203.io>
2024-01-15 02:27:19 -0500

    USB Test application

--- devices/MatrixBlock6_ESP32S3/Applications.h
+++ devices/MatrixBlock6_ESP32S3/Applications.h
@@ -10,6 +10,7 @@
 // USER APPLICATION
 #include "applications/Lighting/Lighting.h"
 // #include "applications/HIDtest/HIDtest.h"
+#include "applications/USBtest/USBtest.h"
 // #include "applications/Example/Example.h"
 
 // BOOT ANIMATION
