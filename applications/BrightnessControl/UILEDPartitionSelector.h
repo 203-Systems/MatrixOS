30aebffd330eb7b6bb3dc770560a7dd52297ee9d
Nengzhuo Cai <null@203.io>
2024-07-25 16:41:04 -0400

    LED Partition Multiplier Control

---
+++ applications/BrightnessControl/UILEDPartitionSelector.h
@@ -0,0 +1,60 @@
+#include "MatrixOS.h"
+
+class UILEDPartitionSelector : public UIComponent {
+ public:
+  uint8_t* selector;
+  std::function<void(uint8_t)> callback;
+  Color color;
+  Color alternative_color;
+
+  UILEDPartitionSelector(Color color, uint8_t* selector,  Color alternative_color = Color(0xFFFFFF), std::function<void(uint8_t)> callback = nullptr){
+    this->selector = selector;
+    this->callback = callback;
+    this->color = color;
+    this->alternative_color = alternative_color;
+  }
+
+  virtual Dimension GetSize() { return Dimension(1 + Device::led_partitions.size(), 1); }
+  virtual Color GetColor() { return color; }
+  virtual Color GetAlternativeColor() { return alternative_color; }
+
+  virtual bool Render(Point origin) {
+    MatrixOS::LED::SetColor(origin, GetColor().ToLowBrightness(*selector == 255));
+    for (uint8_t x = 0; x < Device::led_partitions.size(); x++) {
+      MatrixOS::LED::SetColor(origin + Point(x + 1, 0), GetAlternativeColor().ToLowBrightness(*selector == x));
+    }
+    return true;
+  }
+
+  virtual bool KeyEvent(Point xy, KeyInfo* keyInfo) {
+    if(keyInfo->state == RELEASED && keyInfo->hold == false)
+    {
+      if(xy.x == 0)
+      {
+        *selector = 255;
+      }
+      else if(xy.x <= Device::led_partitions.size())
+      {
+        *selector = xy.x - 1;
+      }
+
+      if(callback)
+      {
+        callback(*selector);
+      }
+    }
+    else if (keyInfo->state == HOLD)
+    {
+      if(xy.x == 0)
+      {
+        MatrixOS::UIInterface::TextScroll("Global Brightness", GetColor());
+      }
+      else if(xy.x <= Device::led_partitions.size())
+      {
+        MatrixOS::UIInterface::TextScroll(Device::led_partitions[xy.x - 1].name, GetAlternativeColor());
+      }
+    }
+    
+    return true;
+  }
+};
