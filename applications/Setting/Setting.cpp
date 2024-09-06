cf6eb9e70efdf5cd63b95704b6a6b0f71cef50e5
Nengzhuo Cai <null@203.io>
2024-09-05 22:07:01 -0400

    Added Factory Reset button in system setting

--- applications/Setting/Setting.cpp
+++ applications/Setting/Setting.cpp
@@ -67,13 +67,60 @@ void Setting::Start() {
   AddUIComponent(enterDfuBtn, Point(0, Device::y_size - 1));
 
   UIButton resetDevice;
-  resetDevice.SetName("Reset Device");
-  resetDevice.SetColor(Color(0xFF0000));
+  resetDevice.SetName("Factory Reset Device");
+  resetDevice.SetColor(Color(0xFF00FF));
   resetDevice.OnPress([]() -> void {
-    Device::NVS::Clear();
-    Device::Reboot();
+    UI confirmResetUI("Confirm Factory Reset", Color(0xFF00FF));
+
+    confirmResetUI.SetPreRenderFunc([]() -> void {
+      // C
+      MatrixOS::LED::SetColor(Point(0, 0), Color(0xFF00FF));
+      MatrixOS::LED::SetColor(Point(1, 0), Color(0xFF00FF));
+      MatrixOS::LED::SetColor(Point(2, 0), Color(0xFF00FF));
+      MatrixOS::LED::SetColor(Point(0, 1), Color(0xFF00FF));
+      MatrixOS::LED::SetColor(Point(0, 2), Color(0xFF00FF));
+      MatrixOS::LED::SetColor(Point(0, 3), Color(0xFF00FF));
+      MatrixOS::LED::SetColor(Point(1, 3), Color(0xFF00FF));
+      MatrixOS::LED::SetColor(Point(2, 3), Color(0xFF00FF));
+
+      // L
+      MatrixOS::LED::SetColor(Point(3, 0), Color(0xFFFFFF));
+      MatrixOS::LED::SetColor(Point(3, 1), Color(0xFFFFFF));
+      MatrixOS::LED::SetColor(Point(3, 2), Color(0xFFFFFF));
+      MatrixOS::LED::SetColor(Point(3, 3), Color(0xFFFFFF));
+      MatrixOS::LED::SetColor(Point(4, 3), Color(0xFFFFFF));
+
+      // R
+      MatrixOS::LED::SetColor(Point(5, 0), Color(0xFF00FF));
+      MatrixOS::LED::SetColor(Point(6, 0), Color(0xFF00FF));
+      MatrixOS::LED::SetColor(Point(5, 1), Color(0xFF00FF));
+      MatrixOS::LED::SetColor(Point(7, 1), Color(0xFF00FF));
+      MatrixOS::LED::SetColor(Point(5, 2), Color(0xFF00FF));
+      MatrixOS::LED::SetColor(Point(6, 2), Color(0xFF00FF));
+      MatrixOS::LED::SetColor(Point(5, 3), Color(0xFF00FF));
+      MatrixOS::LED::SetColor(Point(7, 3), Color(0xFF00FF));
+    });
+
+    UIButton cancelResetBtn;
+    cancelResetBtn.SetName("Cancel");
+    cancelResetBtn.SetColor(Color(0xFF0000));
+    cancelResetBtn.SetSize(Dimension(2, 2));
+    cancelResetBtn.OnPress([&]() -> void { confirmResetUI.Exit(); });
+    confirmResetUI.AddUIComponent(cancelResetBtn, Point(1, 5));
+
+    UIButton confirmResetBtn;
+    confirmResetBtn.SetName("Confirm");
+    confirmResetBtn.SetColor(Color(0x00FF00));
+    confirmResetBtn.SetSize(Dimension(2, 2));
+    confirmResetBtn.OnPress([]() -> void {
+      Device::NVS::Clear();
+      MatrixOS::SYS::Reboot();
+    });
+    confirmResetUI.AddUIComponent(confirmResetBtn, Point(5, 5));
+
+    confirmResetUI.Start();
   });
-  resetDevice.SetEnabled(MatrixOS::UserVar::developer_mode);
+  // resetDevice.SetEnabled(MatrixOS::UserVar::developer_mode);
   AddUIComponent(resetDevice, Point(0, Device::y_size - 2));
 
   // Infomation
