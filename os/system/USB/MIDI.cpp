ef96b99496bb80f3fa37a1a28ea8a3cf0829ea14
Nengzhuo Cai <null@203.io>
2024-09-08 16:58:10 -0400

    Fix case fell though

--- os/system/USB/MIDI.cpp
+++ os/system/USB/MIDI.cpp
@@ -51,6 +51,7 @@ void tud_midi_rx_cb(uint8_t itf) {
         {
           case MIDIv1_SONG_SELECT:
             packet = MidiPacket(port, SongSelect, 1, &raw_packet[2]);
+            break;
           case MIDIv1_MTC_QUARTER_FRAME:
             packet = MidiPacket(port, MTCQuarterFrame, 1, &raw_packet[2]);
             break;
