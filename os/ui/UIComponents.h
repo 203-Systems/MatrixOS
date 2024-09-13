0ed53386901e4451763cc705661a1dd3762fa5cb
Nengzhuo Cai <null@203.io>
2024-09-13 01:34:31 -0400

    UI Toggle

--- os/ui/UIComponents.h
+++ os/ui/UIComponents.h
@@ -3,6 +3,8 @@
 #include "component/UIComponent.h"
 
 #include "component/UIButton.h"
+#include "component/UIToggle.h"
+
 #include "component/UINumModifier.h"
 
 #include "component/UI4pxNumber.h"
---
+++ os/ui/component/UIToggle.h
@@ -0,0 +1,29 @@
+#pragma once
+#include "../UIInterfaces.h"
+
+class UIToggle : public UIButton {
+ public:
+  bool* value;
+
+  UIToggle() {}
+
+  void SetValue(bool* value) { this->value = value; }
+
+  string GetName() override { return name + " " + (*value ? "On" : "Off"); }
+
+  Color GetColor() override {
+    if (color_func) {
+      return color_func();
+    }
+    return color.DimIfNot(*value);
+  }
+
+  bool PressCallback() override {
+    *value = !*value;
+    if (press_callback != nullptr) {
+      press_callback();
+      return true;
+    }
+    return true;
+  }
+};
