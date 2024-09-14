8e93c36637f3e1115ae9d175bb919c390e0001ba
Nengzhuo Cai <null@203.io>
2024-09-14 01:47:14 -0400

    Updated the Application boiler code to work better with new system animation

--- applications/Application.cpp
+++ applications/Application.cpp
@@ -4,15 +4,24 @@
 
 void Application::Start(void* args) {
   this->args = args;
+  MatrixOS::LED::CreateLayer();
   Setup();
   while (true)
   {
     Loop();
+    taskYIELD();
   }
 }
 
 void Application::Exit() {
   End();
-  UI::FadeOut();
+  if(MatrixOS::UserVar::ui_animation)
+  {
+    MatrixOS::LED::Fade();
+    MatrixOS::LED::Fill(0, 0);
+    // Add a delay so it fade to black
+    // This give the user a better sense that they just exited an APP
+    MatrixOS::SYS::DelayMs(crossfade_duration);
+  }
   MatrixOS::SYS::ExitAPP();
 }
