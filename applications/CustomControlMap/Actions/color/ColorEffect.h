d76d9053d3f2d1c433af7b9dec5fde4e6ab679b8
Nengzhuo Cai <null@203.io>
2024-12-09 22:25:30 -0500

    Customizeable Control Map: Color Effect improved

--- applications/CustomControlMap/Actions/color/ColorEffect.h
+++ applications/CustomControlMap/Actions/color/ColorEffect.h
@@ -2,118 +2,128 @@
 
 namespace ColorEffect
 {
-    const char* TAG = "ColorEffect";
+  const char* TAG = "ColorEffect";
 
-    constexpr uint32_t signature = StaticHash("color");
+  constexpr uint32_t signature = StaticHash("color");
 
-    struct ColorEffectData
+  struct ColorEffectData {
+    bool hasDefault;
+    bool hasActivated;
+    uint32_t defaultColor;
+    uint32_t activatedColor;
+  };
+
+  static bool LoadData(cb0r_t actionData, ColorEffectData* data) {
+    cb0r_s cbor_data;
+    if (!cb0r_get_check_type(actionData, 1, &cbor_data, CB0R_INT))
     {
-        bool hasDefault;
-        bool hasActivated;
-        uint32_t defaultColor;
-        uint32_t activatedColor;
-    };
-
-    static bool LoadData(cb0r_t actionData, ColorEffectData* data)
-    {
-        cb0r_s cbor_data;
-        if(!cb0r_get(actionData, 1, &cbor_data) || cbor_data.type != CB0R_INT)
-        {
-            MLOGE(TAG, "Failed to get action data");
-            return false;
-        }
-        data->hasDefault = cbor_data.value & 0x01;
-        data->hasActivated = (cbor_data.value >> 1) & 0x01;
-
-        if(data->hasDefault == false)
-        {
-            data->defaultColor = 0x000000;
-        }
-        if(!cb0r_get(actionData, 2, &cbor_data) || cbor_data.type != CB0R_INT)
-        {
-            MLOGE(TAG, "Failed to get action data");
-            return false;
-        }
-        data->defaultColor = cbor_data.value;
-
-        if(!cb0r_get(actionData, 3, &cbor_data) || cbor_data.type != CB0R_INT)
-        {
-            MLOGE(TAG, "Failed to get action data");
-            return false;
-        }
-        data->activatedColor = cbor_data.value;
-        return true;
+      return false;
     }
-    
+    MLOGD(TAG, "Value1: %d", cbor_data.value);
+    data->hasDefault = cbor_data.value & 0x01;
+    data->hasActivated = (cbor_data.value >> 1) & 0x01;
 
-    static bool KeyEvent(UADRuntime* uadRT, ActionInfo* actionInfo, cb0r_t actionData, KeyInfo* keyInfo)
+    if (data->hasDefault == false)
     {
-        MLOGV(TAG, "KeyEvent");
-        if(keyInfo->state != KeyState::PRESSED && keyInfo->state != KeyState::RELEASED) return false;
-
-        struct ColorEffectData data;
-
-        if(!LoadData(actionData, &data))
-        {
-            MLOGE(TAG, "Failed to load data");
-            return false;
-        }
-
-        if(data.hasActivated == false)
-        {
-            return true;    
-        }
-
-        if(keyInfo->state == KeyState::PRESSED)
-        {
-            MLOGV(TAG, "KeyEvent - PRESSED");
-            if(actionInfo->indexType == ActionIndexType::COORD)
-            {
-                MatrixOS::LED::SetColor(actionInfo->coord, Color(data.activatedColor), 0);
-            }
-            else if(actionInfo->indexType == ActionIndexType::ID)
-            {
-                MatrixOS::LED::SetColor(actionInfo->id, Color(data.activatedColor), 0);
-            }
-            return true;
-        }
-        else if(keyInfo->state == KeyState::RELEASED)
-        {   
-            MLOGV(TAG, "KeyEvent - RELEASED");
-            if(actionInfo->indexType == ActionIndexType::COORD)
-            {
-                MatrixOS::LED::SetColor(actionInfo->coord, Color(data.defaultColor), 0);
-            }
-            else if(actionInfo->indexType == ActionIndexType::ID)
-            {
-                MatrixOS::LED::SetColor(actionInfo->id, Color(data.defaultColor), 0);
-            }
-            return true;
-        }
+      data->defaultColor = 0x000000;
+    }
+    else
+    {
+      if (!cb0r_next_check_type(actionData, &cbor_data, &cbor_data, CB0R_INT))
+      {
         return false;
-    }
-    
+      }
 
-    static void Initialization(UADRuntime* uadRT, ActionInfo* actionInfo, cb0r_t actionData)
+      MLOGD(TAG, "Base Color: %d", cbor_data.value);
+
+      data->defaultColor = cbor_data.value;
+    }
+
+    if (data->hasActivated == false)
     {
-        struct ColorEffectData data;
-
-        if(!LoadData(actionData, &data))
-        {
-            MLOGE(TAG, "Failed to load data");
-            return;
-        }
-
-        if(data.hasDefault)
-        {
-            if(actionInfo->indexType == ActionIndexType::COORD)
-            {
-                MatrixOS::LED::SetColor(actionInfo->coord, Color(data.defaultColor), 0);
-            }
-            else if(actionInfo->indexType == ActionIndexType::ID)
-            {
-                MatrixOS::LED::SetColor(actionInfo->id, Color(data.defaultColor), 0);
-            }
-        }
+      data->activatedColor = 0xFFFFFF;
+      return true;
     }
+    else
+    {
+      if (!cb0r_next_check_type(actionData, &cbor_data, &cbor_data, CB0R_INT))
+      {
+        return false;
+      }
+
+      MLOGD(TAG, "Activated Color: %d", cbor_data.value);
+
+      data->activatedColor = cbor_data.value;
+    }
+    return true;
+  }
+
+  static bool KeyEvent(UADRuntime* uadRT, ActionInfo* actionInfo, cb0r_t actionData, KeyInfo* keyInfo) {
+    MLOGV(TAG, "KeyEvent");
+    if (keyInfo->state != KeyState::PRESSED && keyInfo->state != KeyState::RELEASED)
+      return false;
+
+    struct ColorEffectData data;
+
+    if (!LoadData(actionData, &data))
+    {
+      MLOGE(TAG, "Failed to load data");
+      return false;
+    }
+
+    // if(data.hasActivated == false)
+    // {
+    //     return true;
+    // }
+
+    if (keyInfo->state == KeyState::PRESSED)
+    {
+      MLOGV(TAG, "KeyEvent - PRESSED");
+      if (actionInfo->indexType == ActionIndexType::COORD)
+      {
+        MatrixOS::LED::SetColor(actionInfo->coord, Color(data.activatedColor), 0);
+      }
+      else if (actionInfo->indexType == ActionIndexType::ID)
+      {
+        MatrixOS::LED::SetColor(actionInfo->id, Color(data.activatedColor), 0);
+      }
+      return true;
+    }
+    else if (keyInfo->state == KeyState::RELEASED)
+    {
+      MLOGV(TAG, "KeyEvent - RELEASED");
+      if (actionInfo->indexType == ActionIndexType::COORD)
+      {
+        MatrixOS::LED::SetColor(actionInfo->coord, Color(data.defaultColor), 0);
+      }
+      else if (actionInfo->indexType == ActionIndexType::ID)
+      {
+        MatrixOS::LED::SetColor(actionInfo->id, Color(data.defaultColor), 0);
+      }
+      return true;
+    }
+    return false;
+  }
+
+  static void Initialization(UADRuntime* uadRT, ActionInfo* actionInfo, cb0r_t actionData) {
+    struct ColorEffectData data;
+
+    if (!LoadData(actionData, &data))
+    {
+      MLOGE(TAG, "Failed to load data");
+      return;
+    }
+
+    // if(data.hasDefault)
+    // {
+    if (actionInfo->indexType == ActionIndexType::COORD)
+    {
+      MatrixOS::LED::SetColor(actionInfo->coord, Color(data.defaultColor), 0);
+    }
+    else if (actionInfo->indexType == ActionIndexType::ID)
+    {
+      MatrixOS::LED::SetColor(actionInfo->id, Color(data.defaultColor), 0);
+    }
+    // }
+  }
 };
