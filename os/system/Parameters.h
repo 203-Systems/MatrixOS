3ac4f3739cd6e49c76846be47572b29b0fcfbc67
Nengzhuo Cai <null@203.io>
2025-04-06 17:00:58 -0700

    2.6 Beta 2

--- os/system/Parameters.h
+++ os/system/Parameters.h
@@ -23,7 +23,7 @@
 #define MATRIXOS_MAJOR_VER 2
 #define MATRIXOS_MINOR_VER 6
 #define MATRIXOS_PATCH_VER 0
-#define MATRIXOS_RELEASE_VER 1 //This is for beta etc, doesn't take effect in Stable Release. It should also never be 0
+#define MATRIXOS_RELEASE_VER 2 //This is for beta etc, doesn't take effect in Stable Release. It should also never be 0
 
 
 #include "ReleaseConfig.h"
