0001d3dffc53cc7925c6032c6cc6f645fd40bf16
Nengzhuo Cai <null@203.io>
2025-07-06 19:49:53 -0700

    std::optional for UIComponent

--- os/ui/component/UIComponent.h
+++ os/ui/component/UIComponent.h
@@ -1,10 +1,11 @@
 #pragma once
 #include "MatrixOS.h"
+#include <optional>
 
 class UIComponent {
  public:
   bool enabled = true;
-  std::function<bool()> enable_func;
+  std::optional<std::function<bool()>> enable_func;
 
   virtual Dimension GetSize() { return Dimension(0, 0); }
   virtual bool KeyEvent(Point xy, KeyInfo* keyInfo) { return false; }  //
@@ -14,10 +15,13 @@ class UIComponent {
   void SetEnabled(bool enabled) { this->enabled = enabled; }
   void ShouldEnable(std::function<bool()> enable_func) { this->enable_func = enable_func; }
 
-  bool IsEnabled() { 
-    if (enable_func) { return enable_func(); }
-    return enabled; 
+  bool IsEnabled() {
+    if (enable_func.has_value()) {
+      return (*enable_func)();
+    }
+    return enabled;
   }
+
 
   virtual ~UIComponent(){};
 
