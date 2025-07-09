4177b93783dbc89fac5dee49126a75cb7fc7b806
Nengzhuo Cai <null@203.io>
2025-07-09 10:45:56 -0700

    cmake progress

--- core/ESP32SX/esp32sx.cmake
+++ core/ESP32SX/esp32sx.cmake
@@ -3,3 +3,9 @@ set(SDKCONFIG ${CMAKE_BINARY_DIR}/sdkconfig)
 include($ENV{IDF_PATH}/tools/cmake/project.cmake)
 
 idf_component_get_property( FREERTOS_ORIG_INCLUDE_PATH freertos ORIG_INCLUDE_PATH)
+
+string(TOUPPER "${IDF_TARGET}" IDF_TARGET_UPPER)
+
+target_compile_options(MatrixOS PUBLIC
+  "-DCFG_TUSB_MCU=OPT_MCU_${IDF_TARGET_UPPER}"
+)
