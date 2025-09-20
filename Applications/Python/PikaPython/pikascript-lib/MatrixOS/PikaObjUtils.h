07bbf9b1115e45a7e7eb37f92634b1ef8ba7d762
Nengzhuo Cai <null@203.io>
2025-09-20 01:48:28 -0400

    Updated PikaObjUtils

--- Applications/Python/PikaPython/pikascript-lib/MatrixOS/PikaObjUtils.h
+++ Applications/Python/PikaPython/pikascript-lib/MatrixOS/PikaObjUtils.h
@@ -2,66 +2,33 @@
 
 #include "PikaObj.h"
 
-template <typename T>
-void deleteCppObjInPikaObj(PikaObj* pika_obj) {
-    T* ptr = static_cast<T*>(obj_getPtr(pika_obj, (char*)"_obj_ptr"));
-    if (ptr) {
-        delete ptr;
-        obj_setPtr(pika_obj, (char*)"_obj_ptr", nullptr);
-    }
-}
-
-template <typename T>
-T* createCppObjPtrInPikaObj(PikaObj* pika_obj)
-{
-    deleteCppObjInPikaObj<T>(pika_obj);
-
-    // Allocate raw object with default constructor
-    T* ptr = new T();
-    obj_setPtr(pika_obj, (char*)"_obj_ptr", ptr);
-
-    return ptr;
-}
-
-// Overload for objects that need constructor arguments
 template <typename T, typename... Args>
-T* createCppObjPtrInPikaObj(PikaObj* pika_obj, Args&&... args)
+void createCppObjPtrInPikaObj(PikaObj* pika_obj, Args&&... args)
 {
-    deleteCppObjInPikaObj<T>(pika_obj);
-
-    // Allocate raw object with constructor arguments
-    T* ptr = new T(std::forward<Args>(args)...);
-    obj_setPtr(pika_obj, (char*)"_obj_ptr", ptr);
-
-    return ptr;
+    T obj = T(std::forward<Args>(args)...);
 }
 
 template <typename T>
-T* copyCppObjIntoPikaObj(PikaObj* pika_obj, const T& obj)
+void copyCppObjIntoPikaObj(PikaObj* pika_obj, T& obj)
 {
-    deleteCppObjInPikaObj<T>(pika_obj);
-    
     // Create a copy of the object
-    T* ptr = new T(obj);
-    obj_setPtr(pika_obj, (char*)"_obj_ptr", ptr);
-    
-    return ptr;
+    obj_setStruct(pika_obj, (char*)"_self", obj);
 }
 
 // Convenience functions for safer pointer access
 template <typename T>
 T* getCppObjPtrInPikaObj(PikaObj* pika_obj) {
-    T* ptr = static_cast<T*>(obj_getPtr(pika_obj, (char*)"_obj_ptr"));
+    T* ptr = (T*)(obj_getStruct(pika_obj, (char*)"_self"));
     return ptr;
 }
 
 
 template <typename T>
 T& getCppObjInPikaObj(PikaObj* pika_obj) {
-    T* ptr = static_cast<T*>(obj_getPtr(pika_obj, (char*)"_obj_ptr"));
+    T* ptr = (T*)(obj_getStruct(pika_obj, (char*)"_self"));
     return *ptr;
 }
 
 inline bool hasCppObjInPikaObj(PikaObj* pika_obj) {
-    return (obj_getPtr(pika_obj, (char*)"_obj_ptr")) != nullptr;
+    return (obj_getStruct(pika_obj, (char*)"_self")) != nullptr;
 }
