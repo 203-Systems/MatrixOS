1691c78ea222464a1bd860e366c73d8843d277c1
Nengzhuo Cai <null@203.io>
2025-09-23 21:42:19 -0400

    KeyEvent pass though KeyInfo API

--- OS/Framework/KeyEvent/KeyEvent.h
+++ OS/Framework/KeyEvent/KeyEvent.h
@@ -5,4 +5,13 @@
 struct KeyEvent {
   uint16_t id;
   KeyInfo info;
+
+  // Pass-through functions to KeyInfo
+  KeyState State() { return info.State(); }
+  bool Hold() { return info.Hold(); }
+  uint32_t HoldTime(void) { return info.HoldTime(); }
+  bool Active() { return info.Active(); }
+  operator bool() { return info.operator bool(); }
+  Fract16 Force() const { return info.Force(); }
+  Fract16 Value(uint8_t index = 0) const { return info.Value(index); }
 };
