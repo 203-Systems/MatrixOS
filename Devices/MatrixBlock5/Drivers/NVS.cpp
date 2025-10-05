0e75e62099a2ffcaeb6382b79d82037c8078b5d9
Nengzhuo Cai <null@203.io>
2025-10-05 11:30:20 -0400

    Remove existing NVS

--- Devices/MatrixBlock5/Drivers/NVS.cpp
+++ Devices/MatrixBlock5/Drivers/NVS.cpp
@@ -1,56 +1,32 @@
-#include "NVS.h"
+#include "Device.h"
 
 namespace Device::NVS
 {
-  string tag = "NVS Driver";
-
   void Init() {
-    // TODO: Implement NVS driver for STM32F103
-    // For now, this is stubbed out
+    // Stub: NVS not implemented for STM32F103
   }
 
   void Clear() {
-    // TODO: Implement NVS clear
-  }
-
-  bool Get(string name_space, string key, void* pointer, uint16_t length) {
-    // TODO: Implement NVS get
-    return false;
-  }
-
-  bool Set(string name_space, string key, void* pointer, uint16_t length) {
-    // TODO: Implement NVS set
-    return false;
-  }
-
-  bool GetString(string name_space, string key, string* string_p) {
-    // TODO: Implement NVS get string
-    return false;
-  }
-
-  bool SetString(string name_space, string key, string* string_p) {
-    // TODO: Implement NVS set string
-    return false;
-  }
-
-  // Additional NVS functions required by MatrixOS::NVS
-  std::vector<char> Read(uint32_t hash) {
-    // TODO: Implement NVS read by hash
-    return std::vector<char>();
-  }
-
-  bool Write(uint32_t hash, void* pointer, uint16_t length) {
-    // TODO: Implement NVS write by hash
-    return false;
+    // Stub: NVS not implemented for STM32F103
   }
 
   size_t Size(uint32_t hash) {
-    // TODO: Implement NVS size by hash
+    // Stub: NVS not implemented for STM32F103
     return 0;
   }
 
+  vector<char> Read(uint32_t hash) {
+    // Stub: NVS not implemented for STM32F103
+    return vector<char>();
+  }
+
+  bool Write(uint32_t hash, void* pointer, uint16_t length) {
+    // Stub: NVS not implemented for STM32F103
+    return false;
+  }
+
   bool Delete(uint32_t hash) {
-    // TODO: Implement NVS delete by hash
+    // Stub: NVS not implemented for STM32F103
     return false;
   }
 }
